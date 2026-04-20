14fba24449984196521d728403d4b92fcf661bc3
pepu <pepshove@gmail.com>
2026-04-20 13:31:26 +0200

    fixed s3-backend

--- terraform/s3-backend.tf
+++ terraform/s3-backend.tf
@@ -2,9 +2,10 @@ terraform {
   backend "s3" {
     bucket         = "jerney-tf-state-bucket"
     key            = "jerney/terraform.tfstate"
-    region         = af-south-1
+    region         = "af-south-1"
     encrypt        = true
     # Native S3 locking (Recommended for Terraform 1.10+)
     use_lockfile   = true
   }
 }
+# 
